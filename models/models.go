575bbf981971ea293e2486d8665ee0e143a6eda9
cher21 <if17.cherrynurdin@mhs.ubpkarawang.ac.id>
2023-12-12 14:56:57 +0700

    upload image

--- models/models.go
+++ models/models.go
@@ -6,19 +6,18 @@ import (
 	"fmt"
 	"log"
 	"time"
+	"os"
+	"path/filepath"
 
 	_ "github.com/lib/pq" // postgres golang driver
 )
 
-// Buku schema dari tabel Buku
-// kita coba dengan jika datanya null
-// jika return datanya ada yg null, silahkan pake NullString, contohnya dibawah
-// Penulis       config.NullString `json:"penulis"`
 type Buku struct {
 	ID            int64     `json:"id"`
 	Judul_buku    string    `json:"judul_buku"`
 	Penulis       string    `json:"penulis"`
 	Tgl_publikasi time.Time `json:"tgl_publikasi"`
+	Image         string    `json:"image"`
 }
 
 func TambahBuku(buku Buku) int64 {
@@ -36,13 +35,13 @@ func TambahBuku(buku Buku) int64 {
 
 	// kita buat insert query
 	// mengembalikan nilai id akan mengembalikan id dari buku yang dimasukkan ke db
-	sqlStatement := `INSERT INTO buku (judul_buku, penulis, tgl_publikasi) VALUES ($1, $2, $3) RETURNING id`
+	sqlStatement := `INSERT INTO buku (judul_buku, penulis, tgl_publikasi, image) VALUES ($1, $2, $3, $4) RETURNING id`
 
 	// id yang dimasukkan akan disimpan di id ini
 	var id int64
 
 	// Scan function akan menyimpan insert id didalam id id
-	err := db.QueryRow(sqlStatement, buku.Judul_buku, buku.Penulis, buku.Tgl_publikasi).Scan(&id)
+	err := db.QueryRow(sqlStatement, buku.Judul_buku, buku.Penulis, buku.Tgl_publikasi, buku.Image).Scan(&id)
 
 	if err != nil {
 		log.Fatalf("Tidak Bisa mengeksekusi query. %v", err)
@@ -82,7 +81,7 @@ func AmbilSemuaBuku() ([]Buku, error) {
 		var buku Buku
 
 		// kita ambil datanya dan unmarshal ke structnya
-		err = rows.Scan(&buku.ID, &buku.Judul_buku, &buku.Penulis, &buku.Tgl_publikasi)
+		err = rows.Scan(&buku.ID, &buku.Judul_buku, &buku.Penulis, &buku.Tgl_publikasi, &buku.Image)
 
 		if err != nil {
 			log.Fatalf("tidak bisa mengambil data. %v", err)
@@ -113,7 +112,7 @@ func AmbilSatuBuku(id int64) (Buku, error) {
 	// eksekusi sql statement
 	row := db.QueryRow(sqlStatement, id)
 
-	err := row.Scan(&buku.ID, &buku.Judul_buku, &buku.Penulis, &buku.Tgl_publikasi)
+	err := row.Scan(&buku.ID, &buku.Judul_buku, &buku.Penulis, &buku.Tgl_publikasi, &buku.Image)
 
 	switch err {
 	case sql.ErrNoRows:
@@ -142,10 +141,10 @@ func UpdateBuku(id int64, buku Buku) int64 {
 	buku.Tgl_publikasi = waktuSekarang
 
 	// kita buat sql query create
-	sqlStatement := `UPDATE buku SET judul_buku=$2, penulis=$3, tgl_publikasi=$4 WHERE id=$1`
+	sqlStatement := `UPDATE buku SET judul_buku=$2, penulis=$3, tgl_publikasi=$4, image=$5 WHERE id=$1`
 
 	// eksekusi sql statement
-	res, err := db.Exec(sqlStatement, id, buku.Judul_buku, buku.Penulis, buku.Tgl_publikasi)
+	res, err := db.Exec(sqlStatement, id, buku.Judul_buku, buku.Penulis, buku.Tgl_publikasi, buku.Image)
 
 	if err != nil {
 		log.Fatalf("Tidak bisa mengeksekusi query. %v", err)
@@ -165,12 +164,20 @@ func UpdateBuku(id int64, buku Buku) int64 {
 }
 
 func HapusBuku(id int64) int64 {
-
 	// mengkoneksikan ke db postgres
 	db := config.CreateConnection()
 
 	// kita tutup koneksinya di akhir proses
 	defer db.Close()
+
+	// buat sql query untuk mendapatkan nama file gambar
+	getImageNameSQL := `SELECT image FROM buku WHERE id=$1`
+	var imageName string
+	err := db.QueryRow(getImageNameSQL, id).Scan(&imageName)
+
+	if err != nil && err != sql.ErrNoRows {
+		log.Fatalf("tidak bisa mendapatkan nama file gambar. %v", err)
+	}
 
 	// buat sql query
 	sqlStatement := `DELETE FROM buku WHERE id=$1`
@@ -182,7 +189,7 @@ func HapusBuku(id int64) int64 {
 		log.Fatalf("tidak bisa mengeksekusi query. %v", err)
 	}
 
-	// cek berapa jumlah data/row yang di hapus
+	// cek berapa jumlah data/row yang dihapus
 	rowsAffected, err := res.RowsAffected()
 
 	if err != nil {
@@ -191,5 +198,21 @@ func HapusBuku(id int64) int64 {
 
 	fmt.Printf("Total data yang terhapus %v", rowsAffected)
 
+	// delete the image file if it exists
+	if rowsAffected > 0 {
+		imagePath := filepath.Join("uploads", imageName)
+		err := os.Remove(imagePath)
+		if err != nil {
+			if !os.IsNotExist(err) {
+				log.Printf("tidak bisa menghapus gambar. %v", err)
+			} else {
+				fmt.Printf("Gambar %s tidak ditemukan", imageName)
+			}
+		} else {
+			fmt.Printf("Gambar %s berhasil dihapus", imageName)
+		}
+	}
+
 	return rowsAffected
 }
+
